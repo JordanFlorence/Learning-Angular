4ac6e5d2128db2fa00bb62941b1b7202b5dba1ff
Jordan Florence <W0726143@myscc.ca>
2020-12-02 21:38:07 -0500

    Incomplete Assignment 8

--- src/app/create-content/create-content.component.ts
+++ src/app/create-content/create-content.component.ts
@@ -1,18 +1,19 @@
-import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import {Component, EventEmitter, Output, OnInit, Inject} from '@angular/core';
 import { Content } from '../helper-files/content-interface';
 import { ContentService } from '../services/content.service';
+import {MatDialog, MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-create-component',
-  templateUrl: './create-component.component.html',
-  styleUrls: ['./create-component.component.scss']
+  selector: 'app-create-content',
+  templateUrl: './create-content.component.html',
+  styleUrls: ['./create-content.component.scss']
 })
-export class CreateComponentComponent implements OnInit {
+export class CreateContentComponent implements OnInit {
 
   @Output() newContentEvent = new EventEmitter<Content>();
   @Output() updateContentEvent = new EventEmitter<string>();
   newContent: any;
-  constructor(private contentService: ContentService) {
+  constructor(private contentService: ContentService, public dialog: MatDialog) {
     this.newContent = {
       title: '',
       imageUrl: ''
@@ -20,6 +21,17 @@ export class CreateComponentComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  openInsertContentDialog(): void {
+    const contentDialogRef = this.dialog.open(InsertContentDialog, {
+      width: '400px'
+    });
+    contentDialogRef.afterClosed().subscribe(newContentFromDialog => {
+      this.newContent = newContentFromDialog;
+      this.insertContent();
+    });
+
   }
 
   insertContent(): void{
@@ -39,4 +51,23 @@ export class CreateComponentComponent implements OnInit {
       this.updateContentEvent.emit('Content that was updated, id: ' + this.newContent.id);
     });
   }
+}
+
+@Component({
+  selector: 'app-create-content-dialog',
+  templateUrl: 'create-content.dialog.component.html',
+})
+export class InsertContentDialog {
+  newContent: any;
+  constructor(public dialogRef: MatDialogRef<InsertContentDialog>) {
+    this.newContent = {
+      title: '',
+      imageUrl: ''
+    };
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
 }
